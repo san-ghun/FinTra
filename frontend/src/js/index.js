c33354739a98c3990742beb0bfba699bd8abb673
Sanghun <po4865@gmail.com>
2023-08-22 02:19:52 +0200

    feat: Connect index page to pocketbase index.js - Add functions for buttons and add event listeners to it.
    index.html
    - Add script tags for js-sdk for pocketbase and for syncing data from pocketbase to the page.

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -11,6 +11,9 @@ const section_budget = document.getElementById("budget");
 const tabs = [tab_overview, tab_transactions, tab_budget];
 const sections = [section_overview, section_transactions, section_budget];
 
+const btn_addincome = document.getElementById("toaddincome");
+const btn_addexpense = document.getElementById("toaddexpense");
+
 function show_overview(event) {
   event.preventDefault();
   tabs.forEach((tab) => {
@@ -47,6 +50,19 @@ function show_budget(event) {
   sections[2].hidden = false;
 }
 
+function click_addincome(event) {
+  event.preventDefault();
+  location.assign("/FinTra/frontend/src/income.html");
+}
+
+function click_addexpense(event) {
+  event.preventDefault();
+  location.assign("/FinTra/frontend/src/expense.html");
+}
+
 tab_overview.addEventListener("click", show_overview);
 tab_transactions.addEventListener("click", show_transactions);
 tab_budget.addEventListener("click", show_budget);
+
+btn_addincome.addEventListener("click", click_addincome);
+btn_addexpense.addEventListener("click", click_addexpense);
